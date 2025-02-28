5b81aa4e8599b7589220df96d5c17713b2322235
Daniele Cardullo <danielecardullo.drive@gmail.com>
2025-02-28 16:56:14 +0100

    Bug fix in DGM4 download

--- dgm4_download.py
+++ dgm4_download.py
@@ -1,12 +1,11 @@
 import requests 
 import zipfile
-import shutil
 import os 
 import concurrent.futures
 
 
 os.makedirs('./hf_cache', exist_ok=True)
-path = "hf_cache/data.zip"
+path = "hf_cache/"
 
 os.makedirs('./data', exist_ok=True)
 os.makedirs('./data/DGM4', exist_ok=True)
@@ -16,12 +15,12 @@ os.makedirs('./data/DGM4/origin', exist_ok=True)
 origins = ['washington_post', 'bbc', 'guardian', 'usa_today']   # 'bbc', 'guardian', 'usa_today', 
 manipulations = ['simswap', 'StyleCLIP', 'HFGI', 'infoswap']   # 'HFGI', 'StyleCLIP', 'infoswap', 
 
-def download_and_extract(url, extract_path):
+def download_and_extract(url, extract_path, name):
     response = requests.get(url, stream=True)
-    with open(path, 'wb') as fp:
+    with open(f"{path}/{name}.zip", 'wb') as fp:
         for chunk in response.iter_content(chunk_size=1024):
             fp.write(chunk)
-    with zipfile.ZipFile(path, "r") as zp:
+    with zipfile.ZipFile(f"{path}/{name}.zip", "r") as zp:
         zp.extractall(extract_path)
     print(f"Downloaded and extracted {url}")
 
@@ -29,11 +28,13 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     futures = []
     for origin in origins:
         url = f'https://huggingface.co/datasets/rshaojimmy/DGM4/resolve/main/origin/{origin}.zip'
-        futures.append(executor.submit(download_and_extract, url, './data/DGM4/origin'))
+        futures.append(executor.submit(download_and_extract, url, './data/DGM4/origin', origin))
 
     for manipulation in manipulations:
         url = f'https://huggingface.co/datasets/rshaojimmy/DGM4/resolve/main/manipulation/{manipulation}.zip'
-        futures.append(executor.submit(download_and_extract, url, './data/DGM4/manipulation'))
+        futures.append(executor.submit(download_and_extract, url, './data/DGM4/manipulation', manipulation))
 
     for future in concurrent.futures.as_completed(futures):
         future.result()
+
+os.removedirs("./hf_cache")
