949d0f9e0262f2c79c0ffccbf8ad8b8c09bb1198
Daniele Cardullo <danielecardullo.drive@gmail.com>
2025-03-11 10:35:16 +0100

    yaml with parameters

--- train_bi_dec.py
+++ train_bi_dec.py
@@ -4,29 +4,51 @@ from lightning import Trainer
 from lightning.pytorch.loggers import WandbLogger
 import torch
 from dgm4_download import download_dgm4
+import yaml
 
 
-print("Downloading DGM4")
-download_dgm4()
-print("Dataset Downloaded")
+def main(num_heads, hidden_dim, trainable, epochs, batch_size, grad_acc):
+    print("Downloading DGM4")
+    download_dgm4()
+    print("Dataset Downloaded")
 
-ds_loader = DatasetLoader(batch_size=16)
-train_dl, val_dl = ds_loader.get_dataloaders()
+    ds_loader = DatasetLoader(batch_size=batch_size)
+    train_dl, val_dl = ds_loader.get_dataloaders()
 
-model = BiDec_Model(
-        ds_loader.dp.empty_pixel_values, 
-        ds_loader.dp.empty_input_ids,
-        ds_loader.dp.empty_attn_mask, 
-        768, 
-        16,
-        hidden_dim=2048,
-        trainable=-3
+    model = BiDec_Model(
+            ds_loader.dp.empty_pixel_values, 
+            ds_loader.dp.empty_input_ids,
+            ds_loader.dp.empty_attn_mask, 
+            768, 
+            num_heads,
+            hidden_dim=hidden_dim,
+            trainable=-trainable
+        )
+
+    logger = WandbLogger('BI_DEC_DGM4', project="Thesis_New")
+
+    torch.set_float32_matmul_precision('high')
+    trainer = Trainer(
+        max_epochs=epochs, 
+        logger=logger, 
+        log_every_n_steps=1, 
+        precision='bf16-mixed', 
+        accumulate_grad_batches=grad_acc, 
+        gradient_clip_val=1.0
     )
+    trainer.fit(model, train_dl, val_dl)
 
-logger = WandbLogger('BI_DEC_DGM4', project="Thesis_New")
+    torch.save(model.state_dict(), "./model_state_dict.pth")
 
-torch.set_float32_matmul_precision('high')
-trainer = Trainer(max_epochs=10, logger=logger, log_every_n_steps=1, precision='bf16-mixed', accumulate_grad_batches=32, gradient_clip_val=1.0)
-trainer.fit(model, train_dl, val_dl)
+if __name__=="__main__":
+    with open("training_parameters.yaml", "r") as file:
+        params = yaml.safe_load(file)
 
-torch.save(model.state_dict(), "./model_state_dict.pth")
+    main(
+        num_heads=params["num_heads"],
+        hidden_dim=params["hidden_dim"],
+        trainable=params["trainable"],
+        epochs=params["epochs"],
+        batch_size=params["batch_size"],
+        grad_acc=params["grad_acc"]
+    )
