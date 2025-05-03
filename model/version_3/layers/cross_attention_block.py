bc136ef5c3a2618cdf5a901dce25f6a79c869281
Daniele Cardullo <danielecardullo.drive@gmail.com>
2025-05-03 15:07:41 +0200

    Weighted cross attention

--- model/version_3/layers/cross_attention_block.py
+++ model/version_3/layers/cross_attention_block.py
@@ -1,5 +1,6 @@
 from torch import nn
 import torch.nn.functional as F
+import torch 
 
 
 class CrossAttnBlock(nn.Module):
@@ -9,7 +10,7 @@ class CrossAttnBlock(nn.Module):
         self.self_attn = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
         self.cross_attn_it = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
         self.cross_attn_tm = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
-
+        self.weights = nn.Parameter(torch.randn((1, 3)), requires_grad=True)
 
         self.mlp = nn.Sequential(
             nn.Linear(embed_dim, hidden_dim),
@@ -22,5 +23,14 @@ class CrossAttnBlock(nn.Module):
         z, _ = self.self_attn(z, z, z)
         z_it, _ = self.cross_attn_it(z, z_i, z_i)
         z_tm, _ = self.cross_attn_tm(z, z_m, z_m)
-        z = self.mlp(z_it + z_tm)
+
+        # Apply attention weights
+        w = self.weights.repeat((z.shape[0], 1))
+        lambda_1 = 1 / (w[:, 0] ** 2 + 1e-8)
+        lambda_2 = 1 / (w[:, 1] ** 2 + 1e-8)
+        lambda_3 = 1 / (w[:, 2] ** 2 + 1e-8)
+
+        z_total = lambda_1 * z + lambda_2 * z_it + lambda_3 * z_tm
+
+        z = self.mlp(z_total)
         return z
