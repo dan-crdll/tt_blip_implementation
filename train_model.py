4fb5ab5373872b87a43bdb299cb24683ea1746db
Daniele Cardullo <danielecardullo.drive@gmail.com>
2025-05-20 10:20:29 +0200

    modular version

--- train_model.py
+++ train_model.py
@@ -3,6 +3,8 @@ from model.version_modular.layers.cross_attention_block import create_fusion_lay
 from model.version_modular.architecture import Model
 from torch import nn 
 
+
+
 def create_classifiers():
     print("#### CLASSIFIERS CONFIGURATION ####")
 
@@ -45,4 +47,46 @@ def main():
         bin_classifier,
         multi_classifier,
         lr
+    )
+
+    logger = WandbLogger('BI_DEC_DGM4', project="Thesis_New")
+
+    model.load_partial_weights("./Thesis_New/izkt1qtw/checkpoints/epoch=4-step=1020.ckpt")
+
+    torch.set_float32_matmul_precision('high')
+    
+    train_dl, val_dl = DatasetLoader(origins+manipulations, batch_size).get_dataloaders()
+
+    trainer = L.Trainer(
+        max_epochs=epochs, 
+        logger=logger, 
+        log_every_n_steps=1, 
+        precision='bf16-mixed', 
+        accumulate_grad_batches=grad_acc, 
+        devices=gpus,
+        # strategy='ddp_find_unused_parameters_true',
+        gradient_clip_val=0.7
+    )
+    trainer.fit(model, train_dl, val_dl)
+
+    torch.save(model.state_dict(), "./model_state_dict.pth")
+
+if __name__=="__main__":
+    with open("training_parameters.yaml", "r") as file:
+        params = yaml.safe_load(file)
+
+    main(
+        num_heads=params["num_heads"],
+        hidden_dim=params["hidden_dim"],
+        trainable=params["trainable"],
+        epochs=params["epochs"],
+        batch_size=params["batch_size"],
+        grad_acc=params["grad_acc"],
+        origins=params['origins'],
+        manipulations=params['manipulations'],
+        gpus=params['gpus'],
+        temp=params['temp'],
+        momentum=params['momentum'],
+        queue_size=params['queue_size'],
+        lr=float(params['lr'])
     )
